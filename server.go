33881d8288ea4763f04881b61a4237a7d5e6ae2c
funyug <shivamchawla243@gmail.com>
2017-10-26 13:11:25 +0530

    Refactored timers

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"bitcoin-price-server/controllers"
 	"bitcoin-price-server/exchanges"
 	"bitcoin-price-server/models"
+	"github.com/labstack/echo/middleware"
 	"time"
 )
 
@@ -13,24 +14,46 @@ func main() {
 
 	c,_ := models.New();
 
-	getBitcoinPrices(c,&price);
-	go getPrices(c,&price)
+	getBitcoinPrices(c,&price,[]string{});
+	go getPrices(c,&price,30,[]string{"Zebpay","PocketBits"});
+	go getPrices(c,&price,60,[]string{"Coinsecure"});
+	go getPrices(c,&price,600,[]string{"USDRate"});
 
-	e := echo.New()
+	e := echo.New();
+	e.Use(middleware.Logger());
+	e.Use(middleware.Recover());
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}));
 	e.GET("/bitcoin-price",controllers.GetBitcoinPrice(&price));
 	e.Logger.Fatal(e.Start(":1323"))
 
 }
 
-func getPrices(c *models.Client, price *controllers.BitcoinPrice){
-	for range time.Tick(time.Second *30){
-		getBitcoinPrices(c,price);
+func getPrices(c *models.Client, price *controllers.BitcoinPrice, interval time.Duration, exchanges []string){
+	for range time.Tick(time.Second * interval){
+		getBitcoinPrices(c,price, exchanges);
 	}
 }
 
-func getBitcoinPrices(c *models.Client, price *controllers.BitcoinPrice) {
-	exchanges.GetZebpayPrice(c,price);
-	exchanges.GetCoinSecurePrice(c,price);
-	exchanges.GetPocketBitsPrice(c,price);
-	exchanges.GetUSDRate(c,price);
+func getBitcoinPrices(c *models.Client, price *controllers.BitcoinPrice, exchanges_arr []string) {
+	if len(exchanges_arr) == 0 {
+		exchanges.GetZebpayPrice(c,price);
+		exchanges.GetCoinSecurePrice(c,price);
+		exchanges.GetPocketBitsPrice(c,price);
+		exchanges.GetUSDRate(c,price);
+	} else {
+		for _, v := range exchanges_arr {
+			if v == "Coinsecure" {
+				exchanges.GetCoinSecurePrice(c,price);
+			} else if v == "PocketBits" {
+				exchanges.GetPocketBitsPrice(c,price);
+			} else if v == "Zebpay" {
+				exchanges.GetZebpayPrice(c,price);
+			} else if v == "USDRate" {
+				exchanges.GetUSDRate(c,price);
+			}
+		}
+	}
 }
